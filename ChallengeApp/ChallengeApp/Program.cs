1e6812daf2ed62da20552ba7cea202eebabd21e9
Michał Ogiński <m-oginski00@wp.pl>
2023-04-11 14:02:16 +0200

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Michał";
+var isMale = true;
+var age = 23;
+//(1)
+if (isMale && age < 30)
+{
+    Console.WriteLine("Mężczyzna poniżej 30 lat");
+    //(2)
+    if (isMale && name == "Michał" && age == 23)
+    {
+        Console.WriteLine("Michał, lat 23");
+    }
+}
+//(3)
+else if (!isMale && age < 18)
+{
+    Console.WriteLine("Niepełnoletnia kobieta");
+}
